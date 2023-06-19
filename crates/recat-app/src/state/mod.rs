8c24010923eec9d24164a52bdf09772768268dbe
osamamaqtari <almaktary.osama@gmail.com>
2023-06-19 18:45:21 +0300

    feat(app): add usecase runners

--- crates/recat-app/src/state/mod.rs
+++ crates/recat-app/src/state/mod.rs
@@ -1,6 +1,8 @@
 mod data;
 
 pub use data::DataContext;
+use recat_core::error::AppResult;
+use reddd::domain::UseCase;
 
 /// A trait to represent the state of the application.
 pub trait AppState: Sync {
@@ -8,4 +10,44 @@ pub trait AppState: Sync {
 
     /// Gets the application data context.
     fn data(&self) -> &Self::Data;
+}
+
+#[async_trait::async_trait]
+pub trait UseCaseRunner<U: UseCase> {
+    /// Asynchronously executes runs a usecase of type [`U`].
+    ///
+    /// # Parameters
+    /// - `input` - The input to the use case.
+    ///
+    /// # Returns
+    /// The output of the use case.
+    async fn run_usecase(&self, input: U::Input) -> AppResult<U::Output>;
+}
+
+#[async_trait::async_trait]
+pub trait UseCaseRunnerExt {
+    /// Asynchronously executes runs a usecase of type [`U`].
+    ///
+    /// # Parameters
+    /// - `input` - The input to the use case.
+    ///
+    /// # Returns
+    /// The output of the use case.
+    async fn run<U>(&self, input: U::Input) -> AppResult<U::Output>
+    where
+        U: UseCase,
+        U::Input: Send,
+        Self: UseCaseRunner<U>;
+}
+
+#[async_trait::async_trait]
+impl<S: Sync> UseCaseRunnerExt for S {
+    async fn run<U>(&self, input: U::Input) -> AppResult<U::Output>
+    where
+        U: UseCase,
+        U::Input: Send,
+        Self: UseCaseRunner<U>,
+    {
+        <Self as UseCaseRunner<U>>::run_usecase(self, input).await
+    }
 }
