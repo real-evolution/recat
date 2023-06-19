5ec181a7f157576417dc4c9050ed9c802ecadbf1
osamamaqtari <almaktary.osama@gmail.com>
2023-06-19 18:42:41 +0300

    feat(app): add `AppState` trait

--- crates/recat-app/src/state/mod.rs
+++ crates/recat-app/src/state/mod.rs
@@ -1,3 +1,11 @@
 mod data;
 
 pub use data::DataContext;
+
+/// A trait to represent the state of the application.
+pub trait AppState: Sync {
+    type Data: DataContext;
+
+    /// Gets the application data context.
+    fn data(&self) -> &Self::Data;
+}
