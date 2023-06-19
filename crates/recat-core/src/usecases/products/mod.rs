74d3fe8d4b379f6068a0bd78988e126516916955
osamamaqtari <almaktary.osama@gmail.com>
2023-06-19 19:14:59 +0300

    chore(core): export usecases

--- crates/recat-core/src/usecases/products/mod.rs
+++ crates/recat-core/src/usecases/products/mod.rs
@@ -3,3 +3,9 @@ mod get;
 mod get_page;
 mod remove;
 mod update;
+
+pub use add::{AddProduct, AddProductInput};
+pub use get::GetProduct;
+pub use get_page::GetProductPage;
+pub use remove::RemoveProduct;
+pub use update::{UpdateProduct, UpdateProductInput};
