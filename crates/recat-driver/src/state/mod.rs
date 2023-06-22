ccbb0e3b474d1d20b00b525195ff6eb505c25039
osamamaqtari <almaktary.osama@gmail.com>
2023-06-22 23:53:01 +0300

    feat(driver): add app state implementation

--- crates/recat-driver/src/state/mod.rs
+++ crates/recat-driver/src/state/mod.rs
@@ -1,1 +1,40 @@
 mod data_context;
+
+use recat_app::state::{AppConfig, AppState, DataContext};
+
+use crate::config::RecatArgs;
+
+use self::data_context::RecatDataContext;
+
+pub(crate) type RecatAppState = AppStateImpl<RecatArgs, RecatDataContext>;
+
+pub(crate) struct AppStateImpl<Config, Data> {
+    config: Config,
+    data: Data,
+}
+
+impl<Config, Data> AppState for AppStateImpl<Config, Data>
+where
+    Config: AppConfig,
+    Data: DataContext,
+{
+    type Config = Config;
+    type Data = Data;
+
+    fn config(&self) -> &Self::Config {
+        &self.config
+    }
+
+    fn data(&self) -> &Self::Data {
+        &self.data
+    }
+}
+
+impl RecatAppState {
+    pub(crate) fn new(config: RecatArgs) -> anyhow::Result<Self> {
+        Ok(Self {
+            data: RecatDataContext::new(&config)?,
+            config,
+        })
+    }
+}
